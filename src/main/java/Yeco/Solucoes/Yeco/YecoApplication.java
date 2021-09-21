510cb4dfda96cdd5378c06e2e2edd0a67de71b0e
Bruno Muraro <bmuraro3@gmail.com>
2021-09-21 18:58:33 -0300

    Iniciando o planejamento

---
+++ src/main/java/Yeco/Solucoes/Yeco/YecoApplication.java
@@ -0,0 +1,13 @@
+package Yeco.Solucoes.Yeco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YecoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YecoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Yeco/Solucoes/Yeco/YecoApplicationTests.java
@@ -0,0 +1,13 @@
+package Yeco.Solucoes.Yeco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YecoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
