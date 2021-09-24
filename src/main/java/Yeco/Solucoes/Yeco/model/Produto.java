ea8bbb3e885d6b87ae59e7422796a5b2c712d795
Julio Balbino <julio4110@hotmail.com>
2021-09-24 17:40:17 -0300

    Inicio da Entidade Produto

---
+++ src/main/java/Yeco/Solucoes/Yeco/model/Produto.java
@@ -0,0 +1,71 @@
+package Yeco.Solucoes.Yeco.model;
+
+
+
+
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_produto")
+public class Produto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotNull
+    @Size(min = 5, max = 100)
+    private String nome;
+
+    @NotNull
+    private String descricao;
+
+    @NotNull
+    private Double valor;
+
+    @NotNull
+    private Integer quantidade;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Double getValor() {
+        return valor;
+    }
+
+    public void setValor(Double valor) {
+        this.valor = valor;
+    }
+
+    public Integer getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(Integer quantidade) {
+        this.quantidade = quantidade;
+    }
+}
