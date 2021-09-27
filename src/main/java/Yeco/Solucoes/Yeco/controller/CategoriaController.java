2b8416581e12b50cc99228931f364fd639955276
unknown <marciors92@gmail.com>
2021-09-27 20:25:07 -0300

    CRUD Categoria

--- src/main/java/Yeco/Solucoes/Yeco/controller/CategoriaController.java
+++ src/main/java/Yeco/Solucoes/Yeco/controller/CategoriaController.java
@@ -3,22 +3,69 @@ package Yeco.Solucoes.Yeco.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import Yeco.Solucoes.Yeco.model.Categoria;
 import Yeco.Solucoes.Yeco.repository.CategoriaRepository;
 
-
-
+@RestController
+@RequestMapping("/categorias")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class CategoriaController {
-	
+
 	@Autowired
 	private CategoriaRepository categoriaRepository;
 
 	@GetMapping
-	public ResponseEntity <List<Categoria>> getAll(){
-		return ResponseEntity.ok(categoriaRepository.findAll()); 
+	public ResponseEntity<List<Categoria>> getAll() {
+		return ResponseEntity.ok(categoriaRepository.findAll());
+	}
 
-}
+	@GetMapping("/{id}")
+	public ResponseEntity<Categoria> getById(@PathVariable long id) {
+		return categoriaRepository.findById(id).map(resposta -> ResponseEntity.ok(resposta))
+				.orElse(ResponseEntity.notFound().build());
+
+	}
+	
+	@GetMapping("/nome/{nome}")
+	public ResponseEntity<List<Categoria>> getByNome (@PathVariable String nome) {
+		return ResponseEntity.ok(categoriaRepository.findAllByNomeContainingIgnoreCase(nome));
+	}
+	
+	@GetMapping("/tipo/{tipo}")
+	public ResponseEntity<List<Categoria>> getByTipo (@PathVariable String tipo) {
+		return ResponseEntity.ok(categoriaRepository.findAllByTipoContainingIgnoreCase(tipo));
+	}
+	
+	@GetMapping("/setor/{setor}")
+	public ResponseEntity<List<Categoria>> getBySetor (@PathVariable String setor) {
+		return ResponseEntity.ok(categoriaRepository.findAllBySetorContainingIgnoreCase(setor));
+	}
+	
+	@PostMapping
+	public ResponseEntity<Categoria> postCategoria (@RequestBody Categoria categoria) {
+		return ResponseEntity.status(HttpStatus.CREATED).body(categoriaRepository.save(categoria));
+	}
+	
+	@PutMapping
+	public ResponseEntity<Categoria> putCategoria (@RequestBody Categoria categoria) {
+		return ResponseEntity.status(HttpStatus.OK).body(categoriaRepository.save(categoria));
+	}
+	
+	@DeleteMapping("/{id}")
+	public void deletePostagem (@PathVariable long id) {
+		categoriaRepository.deleteById(id);
+	}
+
 }
