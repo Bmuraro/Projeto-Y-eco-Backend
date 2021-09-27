71dd26e744dba656b36294a4d48a4ad6dc9986fd
Julio Balbino <julio4110@hotmail.com>
2021-09-27 18:09:04 -0300

    Adicionando tabelas em prduto

--- src/main/java/Yeco/Solucoes/Yeco/controller/CategoriaController.java
+++ src/main/java/Yeco/Solucoes/Yeco/controller/CategoriaController.java
@@ -2,12 +2,13 @@ package Yeco.Solucoes.Yeco.controller;
 
 import java.util.List;
 
+import Yeco.Solucoes.Yeco.model.Categoria;
+import Yeco.Solucoes.Yeco.repository.CategoriaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import Yeco.Solucoes.Yeco.model.Categoria;
-import Yeco.Solucoes.Yeco.repository.CategoriaRepository;
+
 
 
 
